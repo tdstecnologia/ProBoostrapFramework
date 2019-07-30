eb84b37ebd437bcca8e822ce3e5a1881b28be3f2
Dherkyan Ribeiro <dherkyan@gmail.com>
2019-07-30 17:58:26 -0400

    Projeto ProBootstrapFramework

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProBoostrapFramework.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+
+        public IActionResult StartTemplate() => View();
+
+        public IActionResult Base() => View();
+
+        public IActionResult MediaQueries() => View();
+
+        public IActionResult ContainerRows() => View();
+
+        public IActionResult Grid() => View();
+
+        public IActionResult AlinhamentoOffset() => View();
+
+        public IActionResult Paginador() => View();
+
+        public IActionResult CheckRadio() => View();
+
+        public IActionResult LoginAmericanas() => View();
+
+        public IActionResult Formulario() => View();
+        public IActionResult Tabela() => View();
+        public IActionResult Imagens() => View();
+        public IActionResult Figure() => View();
+        public IActionResult FonteAwesome() => View();
+        public IActionResult Cards() => View();
+        public IActionResult Navbar() => View();
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ProBoostrapFramework
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(rotas =>{
+                rotas.MapRoute(name:"default", template:"{controller=Home}/{action=Index}");
+            });
+        }
+    }
+}
